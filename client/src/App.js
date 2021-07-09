95fb17f1eccb357efd1c8cd478a157568c7297fa
lerisse <tristanlerisse@gmail.com>
2021-07-08 22:39:48 -0400

    idk

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+// import './App.css';
+// import SpotifyLogin from './SpotifyLogin';
+// import Visualizer from './Visualizer'
+// import Radio from './Radio';
+
+function App() {
+  return (
+      <div>
+        <Visualizer />
+        {/* <Radio /> */}
+      </div>
+    )
+}
+
+export default App;
